bb202b7d3d1af524ba08c432c3d910bd372069eb
Ajay Narayanan <the.ajay.great@gmail.com>
2020-03-29 12:23:01 +0530

    Ccompleted get_data to collate data as  np array

--- data_preparation.py
+++ data_preparation.py
@@ -12,18 +12,106 @@ def read_image(filename, byteorder='>'):
     with open(filename,'rb') as f:
         buffer = f.read()
         # We extract the header, width, height and maxvalue of the image
-        header, width, height, maxvalue = re.search(b"(^P5\s(?:\s*#.*[\r\n])*"
-        b"(\d+)\s(?:\s*#.*[\r\n])*"
-        b"(\d+)\s(?:\s*#.*[\r\n])*"
-        b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
-        # Convert image to numpy array using np.frombuffer 
-        image_numpy = np.frombuffer(buffer, dtype='u1' if int(maxvalue)<256 else byteorder+'u2',
+        try:
+            header, width, height, maxvalue = re.search(b"(^P5\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n])*"
+            b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
+            image_numpy = np.frombuffer(buffer, dtype='u1' if int(maxvalue)<256 else byteorder+'u2',
                                     count = int(width)* int(height),
                                     offset = len(header)
                                     ).reshape(int(height), int(width))
-        return image_numpy
+            return image_numpy
+        except:
+            pass
+        # Convert image to numpy array using np.frombuffer 
+        
+        
 
 def test_read_image():
-    img = Image.open('Data\s1\1.pgm')
-    img = read_image('Data\s1\1.pgm')
+    img = Image.open('Data\\s1\\1.pgm')
+    # img.show()
+    img = read_image('Data\\s1\\1.pgm', 'rw+')
+    print(img)
     print(img.shape)
+
+test_read_image()
+size = 2
+total_sample_size = 10000
+
+def get_data(size, total_sample_size):
+    #read the image
+    image = read_image('Data/s' + str(1) + '/' + str(1) + '.pgm', 'rw+')
+    #reduce the size
+    image = image[::size, ::size]
+    #get the new size
+    dim1 = image.shape[0]
+    dim2 = image.shape[1]
+
+    count = 0
+    
+    #initialize the numpy array with the shape of [total_sample, no_of_pairs, dim1, dim2]
+    x_geuine_pair = np.zeros([total_sample_size, 2, 1, dim1, dim2])  # 2 is for pairs
+    y_genuine = np.zeros([total_sample_size, 1])
+    
+    for i in range(40):
+        for j in range(int(total_sample_size/40)):
+            ind1 = 0
+            ind2 = 0
+            
+            #read images from same directory (genuine pair)
+            while ind1 == ind2:
+                ind1 = np.random.randint(10)
+                ind2 = np.random.randint(10)
+            
+            # read the two images
+            img1 = read_image('Data/s' + str(i+1) + '/' + str(ind1 + 1) + '.pgm', 'rw+')
+            img2 = read_image('Data/s' + str(i+1) + '/' + str(ind2 + 1) + '.pgm', 'rw+')
+            
+            #reduce the size
+            img1 = img1[::size, ::size]
+            img2 = img2[::size, ::size]
+            
+            #store the images to the initialized numpy array
+            x_geuine_pair[count, 0, 0, :, :] = img1
+            x_geuine_pair[count, 1, 0, :, :] = img2
+            
+            #as we are drawing images from the same directory we assign label as 1. (genuine pair)
+            y_genuine[count] = 1
+            count += 1
+
+    count = 0
+    x_imposite_pair = np.zeros([total_sample_size, 2, 1, dim1, dim2])
+    y_imposite = np.zeros([total_sample_size, 1])
+    
+    for i in range(int(total_sample_size/10)):
+        for j in range(10):
+            
+            #read images from different directory (imposite pair)
+            while True:
+                ind1 = np.random.randint(40)
+                ind2 = np.random.randint(40)
+                if ind1 != ind2:
+                    break
+                    
+            img1 = read_image('Data/s' + str(ind1+1) + '/' + str(j + 1) + '.pgm', 'rw+')
+            img2 = read_image('Data/s' + str(ind2+1) + '/' + str(j + 1) + '.pgm', 'rw+')
+
+            img1 = img1[::size, ::size]
+            img2 = img2[::size, ::size]
+
+            x_imposite_pair[count, 0, 0, :, :] = img1
+            x_imposite_pair[count, 1, 0, :, :] = img2
+            #as we are drawing images from the different directory we assign label as 0. (imposite pair)
+            y_imposite[count] = 0
+            count += 1
+            
+    #now, concatenate, genuine pairs and imposite pair to get the whole data
+    X = np.concatenate([x_geuine_pair, x_imposite_pair], axis=0)/255
+    Y = np.concatenate([y_genuine, y_imposite], axis=0)
+
+    return X, Y
+
+X,Y = get_data(size, total_sample_size)
+
+print(X.shape, Y.shape)
