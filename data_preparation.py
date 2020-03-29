3d2c9bf793f6d1eeab4d56b913b295725069b7fb
Ajay Narayanan <the.ajay.great@gmail.com>
2020-03-29 09:58:53 +0530

    Initial commit and data

---
+++ data_preparation.py
@@ -0,0 +1,8 @@
+# Helper functions to arrange the dataset into the format required for training the 
+# siamese networks
+
+# Importing the libraries
+
+import re
+import numpy as np
+from PIL import Image
