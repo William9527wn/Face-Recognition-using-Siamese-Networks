c2383c25a34410e329c8acea0c984c677f539087
Ajay Narayanan <the.ajay.great@gmail.com>
2020-03-29 10:28:43 +0530

    Implementing read_image to convert pgm file to np

--- data_preparation.py
+++ data_preparation.py
@@ -6,3 +6,24 @@
 import re
 import numpy as np
 from PIL import Image
+
+def read_image(filename, byteorder='>'):
+    # read raw file to buffer
+    with open(filename,'rb') as f:
+        buffer = f.read()
+        # We extract the header, width, height and maxvalue of the image
+        header, width, height, maxvalue = re.search(b"(^P5\s(?:\s*#.*[\r\n])*"
+        b"(\d+)\s(?:\s*#.*[\r\n])*"
+        b"(\d+)\s(?:\s*#.*[\r\n])*"
+        b"(\d+)\s(?:\s*#.*[\r\n]\s)*)", buffer).groups()
+        # Convert image to numpy array using np.frombuffer 
+        image_numpy = np.frombuffer(buffer, dtype='u1' if int(maxvalue)<256 else byteorder+'u2',
+                                    count = int(width)* int(height),
+                                    offset = len(header)
+                                    ).reshape(int(height), int(width))
+        return image_numpy
+
+def test_read_image():
+    img = Image.open('Data\s1\1.pgm')
+    img = read_image('Data\s1\1.pgm')
+    print(img.shape)
